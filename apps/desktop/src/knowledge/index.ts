93394b9c7f34bfb22d5e4592fca3fbb2377f0e00
LucasHonoratoDeSouza <lucashonoratodesouza10@gmail.com>
2026-07-25 21:39:48 -0300

    fix(graph): harden fluid interaction scheduling

--- apps/desktop/src/knowledge/index.ts
+++ apps/desktop/src/knowledge/index.ts
@@ -2,28 +2,6 @@ export { ipcKnowledgeClient } from "./ipc";
 export { KnowledgeGraph } from "./KnowledgeGraph";
 export { DEFAULT_LAYOUT_OPTIONS, layoutGraph, radiusFor } from "./forceLayout";
 export type { LayoutNode } from "./forceLayout";
-export {
-  createGraphSimulation,
-  dragGraphNode,
-  reheatGraphSimulation,
-  releaseGraphNode,
-  settleGraphSimulation,
-  tickGraphSimulation,
-} from "./forceSimulation";
-export type {
-  GraphSimulation,
-  SimulationLink,
-  SimulationNode,
-} from "./forceSimulation";
-export {
-  DEFAULT_GRAPH_VIEWPORT,
-  MAX_GRAPH_SCALE,
-  MIN_GRAPH_SCALE,
-  panViewportByPixels,
-  viewBoxFor,
-  zoomViewportAt,
-} from "./graphViewport";
-export type { GraphViewport, NormalizedPoint } from "./graphViewport";
 export type {
   AssistantAnswer,
   CaptureRequest,
